b1885c75e241451dee0afc258fec1dc5e39a6212
Muhammad Rehan <rehanch642@gmail.com>
2022-10-18 22:40:37 +0500

    fix: add transaction comment

--- deploy.js
+++ deploy.js
@@ -3,17 +3,20 @@ const fs = require("fs");
 
 const main = async () => {
 // rpc request HTTP://127.0.0.1:8545
- const provider = new ethers.providers.JsonRpcBatchProvider("http://127.0.0.1:8545");
- const wallet = new ethers.Wallet("974571d3865d1cced730eb43cee2e639c75d430ebf3a2728e03b1dc45d210393", 
+ const provider = new ethers.providers.JsonRpcBatchProvider("http://127.0.0.1:7545");
+ const wallet = new ethers.Wallet("60b5075ca116b751b5b92245c52a500abe087643939efd4f76f1fbbbcc9e1d00", 
  provider)
  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
  const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
  console.log("Deploying.... Please wait....");
+//  deploy contract
  const contract = await contractFactory.deploy();
- console.log(contract);
+//  get receipt when atleast 1 node confirm it
  const deploymentReceipt = await contract.deployTransaction.wait(1);
- console.log(deploymentReceipt);
+ console.log(contract.deployTransaction)
+
+
 
 }
 main()
